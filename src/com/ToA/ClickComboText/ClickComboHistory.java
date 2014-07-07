d1332310e516ee050630f2eb0fdb283430ce0429
SirSlender <michael.suggs@live.com>
2014-07-07 19:31:13 -0400

    - Updated - null shows for first click - Clicks don't concatonate.

---
+++ src/com/ToA/ClickComboText/ClickComboHistory.java
@@ -0,0 +1,78 @@
+package com.ToA.ClickComboText;
+
+import java.util.ArrayList;
+
+import com.sucy.skill.click.MouseClick;
+
+public class ClickComboHistory {
+
+    private static final long limit = 1000;
+
+    private final ArrayList<MouseClick> entries = new ArrayList<MouseClick>();
+    private final ArrayList<String> clicks = new ArrayList<String>();
+    private final ClickComboText cct;
+    
+    private long timer;
+    
+    public ClickComboHistory(ClickComboText plugin)
+    {
+    	this.cct = plugin;
+    }
+    
+    public void addClick(MouseClick click)
+    {
+    	entries.add(click);
+    	clicks.add(String.valueOf(click));
+    	updateEntries();
+        if (entries.size() == 0) entries.add(click);
+    }
+    
+    private void updateEntries()
+    {
+
+        if (entries.size() > 0 && System.currentTimeMillis() - timer > limit) {
+            entries.clear();
+            clicks.clear();
+        }
+        else if (entries.size() == 4)
+        {
+        	entries.clear();
+        }
+        timer = System.currentTimeMillis();
+    }
+    
+    public String concatStrings()
+    {
+    	String concatString = null;
+    	for (int i = 0 ; i < clicks.size() ; i++)
+    	{
+    		if (clicks.get(0).isEmpty())
+    		{
+    			concatString = null;
+    		}
+    		else
+    		{
+    			if (clicks.get(0).equals("LEFT") || clicks.get(0).equals("RIGHT"))
+    			{
+    				concatString = clicks.get(i);
+    			}
+    			else if (clicks.get(i).equals("LEFT") || clicks.get(i).equals("RIGHT"))
+    			{
+    				concatString = concatString + " - " + clicks.get(i);
+    			}
+    		}
+    	}
+    	return concatString;
+    }
+    
+    public void clearClicks()
+    {
+    	clicks.clear();
+    }
+    
+    public int clickSize()
+    {
+    	return clicks.size();
+    }
+
+}
--- src/com/ToA/ClickComboText/ClickComboText.java
+++ src/com/ToA/ClickComboText/ClickComboText.java
@@ -12,9 +12,11 @@ import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.sucy.skill.click.MouseClick;
+
 public class ClickComboText extends JavaPlugin implements Listener
 {
-	private final HashMap<String, Integer> history = new HashMap<String, Integer>();
+	private final HashMap<String, ClickComboHistory> history = new HashMap<String, ClickComboHistory>();
 	
 	@Override
 	public void onEnable()
@@ -33,125 +35,37 @@ public class ClickComboText extends JavaPlugin implements Listener
     public void clickComboText(PlayerInteractEvent e)
     {
     	Player p = e.getPlayer();
-    	p.sendMessage("clickComboText Event fired!");
-    	final HashMap<String, String> click = new HashMap<String, String>();
-    	int ca = clickAmount(p);
-    	String pName = p.getName();
+        if (!history.containsKey(e.getPlayer().getName())) {
+            history.put(e.getPlayer().getName(), new ClickComboHistory(this));
+        }
+        ClickComboHistory pch = history.get(e.getPlayer().getName());
     	ItemMessage im = new ItemMessage(this);
-    	p.sendMessage("Variables initialized!");
     	try{
-    	if (e.getAction() == Action.LEFT_CLICK_AIR || e.getAction() == Action.LEFT_CLICK_BLOCK) {
-        	p.sendMessage("LEFT-CLICK");
-    		if (ca == 0)
-    		{
-    			click.put(pName, "LEFT");
-    			String playerClick = click.get(pName);
-    			im.sendMessage(p, playerClick, 2);
-    			addClicks(p);
-    			e.setCancelled(true);
-    		}
-    		else if (ca > 0 && ca < 5)
-    		{
-    			String prevClicks = click.get(pName);
-    			String newClicks = prevClicks + " - " + "LEFT";
-    			click.put(pName, newClicks);
-    			im.sendMessage(p, newClicks, 2);
-    			addClicks(p);
-    			int newCA = clickAmount(p);
-    			if (newCA == 4)
-    			{
-    				click.remove(pName);
-    			}
-    			e.setCancelled(true);
-    		}
-    		else
-    		{
-    			click.put(pName, "LEFT");
-    			String playerClick = click.get(pName);
-    			im.sendMessage(p, playerClick, 2);
-    			setClick(p, 1);
-    			e.setCancelled(true);
-    		}
-    	}
-    	else if (e.getAction() == Action.RIGHT_CLICK_AIR || e.getAction() == Action.RIGHT_CLICK_BLOCK)
-		{
-    		if (ca == 0)
-    		{
-    			click.put(pName, "RIGHT");
-    			String playerClick = click.get(pName);
-    			im.sendMessage(p, playerClick, 2);
-    			addClicks(p);
-    			e.setCancelled(true);
-    		}
-    		else if (ca > 0 && ca < 5)
-    		{
-    			String prevClicks = click.get(pName);
-    			String newClicks = prevClicks + " - " + "RIGHT";
-    			click.put(pName, newClicks);
-    			im.sendMessage(p, newClicks, 2);
-    			addClicks(p);
-    			int newCA = clickAmount(p);
-    			if (newCA == 4)
-    			{
-    				click.remove(pName);
-    			}
-    			e.setCancelled(true);
-    		}
-    		else
-    		{
-    			click.put(pName, "RIGHT");
-    			String playerClick = click.get(pName);
-    			im.sendMessage(p, playerClick, 2);
-    			setClick(p, 1);
-    			e.setCancelled(true);
-    		}
-		}
+	    	if (e.getAction() == Action.LEFT_CLICK_AIR || e.getAction() == Action.LEFT_CLICK_BLOCK) 
+	    	{
+	        	pch.addClick(MouseClick.LEFT);
+	        	im.sendMessage(p, pch.concatStrings(), 1);
+	        	if (pch.clickSize() >= 4)
+	        	{
+	        		pch.clearClicks();
+	        	}
+	        	e.setCancelled(true);
+	    	}
+	    	else if (e.getAction() == Action.RIGHT_CLICK_AIR || e.getAction() == Action.RIGHT_CLICK_BLOCK)
+			{
+	    		pch.addClick(MouseClick.RIGHT);
+	        	im.sendMessage(p, pch.concatStrings(), 1);
+	        	if (pch.clickSize() >= 4)
+	        	{
+	        		pch.clearClicks();
+	        	}
+	        	e.setCancelled(true);
+			}
     	}
     	catch (NullPointerException e2)
     	{
     		e2.printStackTrace();
     	}
-    }
-    
-    public int clickAmount(Player player)
-    {
-    	int i;
-    	Player p = player;
-    	String pName = p.getName();
-    	if (history.containsKey(pName))
-    	{
-    		i = history.get(pName);
-    		if (i >= 4)
-    		{
-    			i = 0;
-    		}
-    	}
-    	else 
-    	{
-    		i = 0;
-    	}
-    	return i;
-    	
-    }
-    
-    public void addClicks(Player player)
-    {
-    	Player p = player;
-    	String pName = p.getName();
-    	if (history.containsKey(pName))
-    	{
-    		history.put(pName, history.get(pName) + 1);
-    	}
-    	else
-    	{
-    		history.put(pName, 1);
-    	}
-    }
-    
-    public void setClick(Player player, int amount)
-    {
-    	String pName = player.getName();
-    	history.put(pName, amount);
     }
 
     @EventHandler (priority = EventPriority.MONITOR, ignoreCancelled = true)
